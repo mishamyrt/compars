bf7f6f995cc0facc7a711b72985aab27aedfb8ce
mishamyrt <misha@myrt.co>
2021-05-30 20:42:39 +0300

    Fix codestyle error

--- pkg/symbols/extensions.go
+++ pkg/symbols/extensions.go
@@ -21,7 +21,7 @@ var ExtensionsCommentSet = map[string]types.CommentSymbolSet{
 	"bash": CommentSymbols["sh"],
 }
 
-// GetSetByExtension returns comment symbol set for the extension
+// GetSetByExtension returns comment symbol set for the extension.
 func GetSetByExtension(ext string) (types.CommentSymbolSet, error) {
 	if val, ok := ExtensionsCommentSet[ext[1:]]; ok {
 		return val, nil
